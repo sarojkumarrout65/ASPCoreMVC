242299122a7b927e893f8d8f6b6cddb525405f21
skrout <skrout@10-L-PG00ZQAG>
2022-05-24 00:54:16 +0530

    Add project files.

---
+++ ASPCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using ASPCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration _configuration, ILogger<HomeController> logger)
+        {
+           _logger = logger;
+            configuration = _configuration;            
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.AppSetting = configuration["MyKey"];
+            _logger.LogInformation("Test Logger");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
